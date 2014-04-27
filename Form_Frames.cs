fb2b86667cad72cedf6c3087a3fdc7afe58e4f66
Mike Tarkus <mike@tarkus.co>
2014-04-26 20:05:17 -0400

    first commit

---
+++ Form_Frames.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace WebMCam
+{
+	public partial class Form_Frames : Form
+	{
+		string directory_path;
+		string file_format;
+		
+		public Form_Frames(String _directory_path)
+		{
+			InitializeComponent();
+			directory_path = _directory_path;
+		}
+		
+		void Form_FramesLoad(object sender, EventArgs e)
+		{
+			directory_path = directory_path + "\\";
+			var files = Directory.GetFiles(directory_path, "*.png");
+			
+			// Easiest way to sort files numerically
+			file_format = files[1].Split('.')[1];
+			
+			var frame_list = new List<Int16> {};
+			
+			for(int i = 0; i < files.Length; i++)
+				frame_list.Add(Convert.ToInt16(files[i].Replace(directory_path, "").Replace("." + file_format, "")));
+			
+			frame_list.Sort();
+			
+			foreach(var frame in frame_list)
+				list_frames.Items.Add(Convert.ToString(frame));
+			
+			list_frames.SetSelected(0, true);
+		}
+		
+		void List_framesSelectedIndexChanged(object sender, EventArgs e)
+		{
+			try {
+				var bmp = new FileStream(directory_path + list_frames.SelectedItem + "." + file_format, FileMode.Open, FileAccess.Read);
+				picture_frame.Image = Image.FromStream(bmp);
+				bmp.Close();
+			} catch (Exception ex) {
+				list_frames.Items.RemoveAt(list_frames.SelectedIndex);
+			}
+		}
+		
+		void DeleteToolStripMenuItemClick(object sender, EventArgs e)
+		{
+			try { // If can't do it just forget about it, not a big deal
+				
+				for(var i = list_frames.SelectedIndices.Count - 1; i >= 0; i--)
+				{
+					var index = list_frames.SelectedIndices[i];
+					File.Delete(directory_path + list_frames.Items[index] + "." + file_format);
+					list_frames.Items.RemoveAt(index);
+				}
+			} catch { }
+		}
+		
+		void Btn_doneClick(object sender, System.EventArgs e)
+		{
+			Close();
+		}
+		
+		void List_framesKeyDown(object sender, KeyEventArgs e)
+		{
+			if (e.KeyValue == (char)Keys.Delete)
+				DeleteToolStripMenuItemClick(sender, e);
+		}
+	}
+}
---
+++ Form_Settings.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.IO;
+using System.Diagnostics;
+using System.Windows.Forms;
+
+namespace WebMCam
+{
+	/// <summary>
+	/// Description of Form_Settings.
+	/// </summary>
+	public partial class Form_Settings : Form
+	{
+		public Form_Settings()
+		{
+			InitializeComponent();
+		}
+		
+		void Form_SettingsLoad(object sender, System.EventArgs e)
+		{
+			text_ffmpeg.Text = Ini_File.Read("Loc", "ffmpeg");
+			text_temp.Text = Ini_File.Read("Loc", "temp");
+			text_args.Text = Ini_File.Read("Cmd", "args");
+			combo_image_format.Text = Ini_File.Read("Fmt", "image");
+			combo_pixel_format.Text = Ini_File.Read("Fmt", "pixel");
+			chk_delete_frames.Checked = Ini_File.Read("Fmt", "delete") == "True";
+		}
+		
+		void Btn_cancelClick(object sender, System.EventArgs e)
+		{
+			Close();
+		}
+		
+		void Btn_saveClick(object sender, System.EventArgs e)
+		{
+			Ini_File.Write("Loc", "ffmpeg", text_ffmpeg.Text);
+			Ini_File.Write("Loc", "temp", text_temp.Text);
+			Ini_File.Write("Cmd", "args", text_args.Text);
+			Ini_File.Write("Fmt", "image", combo_image_format.Text);
+			Ini_File.Write("Fmt", "pixel", combo_pixel_format.Text);
+			Ini_File.Write("Fmt", "delete", Convert.ToString(chk_delete_frames.Checked));
+			Close();
+		}
+		
+		void Btn_open_tempClick(object sender, System.EventArgs e)
+		{
+			Process.Start(text_temp.Text);
+		}
+		void Btn_resetClick(object sender, System.EventArgs e)
+		{
+			File.Delete(Ini_File.path);
+			Process.Start(Application.ExecutablePath);
+		    Environment.Exit(-1);
+		}
+	}
+}
