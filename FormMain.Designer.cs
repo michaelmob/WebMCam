3735b0866954bb0745ba1eba2f72c697549bbbf2
michael <thetarkus@users.noreply.github.com>
2018-08-10 00:22:38 -0400

    Disable auto scale mode
    Prevent DPI and different font sizes from messing up form layout

--- FormMain.Designer.cs
+++ FormMain.Designer.cs
@@ -297,8 +297,7 @@
             // 
             // FormMain
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.None;
             this.ClientSize = new System.Drawing.Size(477, 325);
             this.Controls.Add(this.buttonWarning);
             this.Controls.Add(this.textBoxSize);
